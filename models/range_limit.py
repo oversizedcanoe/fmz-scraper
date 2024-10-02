d9261eb6c4c7263eff574f78295aff2ecfd5f2b6
billy <billy.cook12345@gmail.com>
2024-10-01 21:42:20 -0400

    Improved  layout of website and begun adding inputs

--- models/range_limit.py
+++ models/range_limit.py
@@ -4,8 +4,6 @@ class RangeLimit:
     def __init__(self, start: datetime, end: datetime) -> None:
         self.start_date = start.date()
         self.end_date = end.date()
-        self.start_string = self.start_date.strftime("%d-%b-%Y")
-        self.end_string = self.end_date.strftime("%d-%b-%Y")
 
     def __str__(self) -> str:
         return f'{self.start.day}/{self.start.month}/{self.start.year} - {self.end.day}/{self.end.month}/{self.end.year}'
--- serve.py
+++ serve.py
@@ -11,4 +11,17 @@ def index():
     with open(f'output/{current_year}.json', 'r') as file:
         zone_limits = json.load(file)
 
+
+    # Add extra fields to API JSON
+    for zone_limit in zone_limits:
+        for fish_limit in zone_limit['fish_limits']:
+            fish_limit['range_strings'] = []
+            if len(fish_limit['limits']) == 0:
+                fish_limit['range_strings'].append("N/A")
+            else:
+                for range_limit in fish_limit['limits']:
+                    start = datetime.strptime(range_limit['start_date'], "%Y-%m-%d")
+                    end = datetime.strptime(range_limit['end_date'], "%Y-%m-%d")
+                    fish_limit['range_strings'].append(f'{start.strftime("%b-%d")} → {end.strftime("%b-%d")}')
+
     return render_template('index.html', zone_limits=zone_limits)
